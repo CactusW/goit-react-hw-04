2d4e7b6f885b924ad944f068189842b9a0dc51d3
Nick <chernyavskij5@gmai.com>
2024-11-12 00:04:53 +0200

    fix_1

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchArticles = async (query, page = 1) => {
     const { data } = await axios.get(`https://api.unsplash.com/search/photos/`, {
       params: {
-        client_id: 'FZK5K8CibqNmzzFrZMI9trJWic4JElwNhSKQb-6L8Jk',
+        client_id: '-_S48f3lnop3CL5HuRCnqVRdn5Ygiefpa0BWdyrHCRg',
         query: query,
         page: page,
         per_page: 12, 
